c93cd0caea5f77e7f214b98a691e90340a2e2d45
Cao Hung <caophihung94@gmail.com>
2020-08-18 14:09:03 +0700

    fix

--- src/HungCP/PhpSimpleHtmlDom/HtmlDomParser.php
+++ src/HungCP/PhpSimpleHtmlDom/HtmlDomParser.php
@@ -7,10 +7,10 @@ require 'lib'.DIRECTORY_SEPARATOR.'simple_html_dom.php';
 class HtmlDomParser {
     
     static public function file_get_html() {
-        return call_user_func_array('\simple_html_dom\file_get_html' , func_get_args());
+        return call_user_func_array('file_get_html' , func_get_args());
     }
 
     static public function str_get_html() {
-        return call_user_func_array('\simple_html_dom\str_get_html' , func_get_args());
+        return call_user_func_array('str_get_html' , func_get_args());
     }
 }
